9dd7c3523b58428e7cdcdacea2cca202af455c07
Yuki Shichiku <yuuki.79@gmail.com>
2014-06-17 01:18:23 +0900

    Added display shaked data

--- public/js/shaked.js
+++ public/js/shaked.js
@@ -1,4 +1,4 @@
-/* global io */
+/* global io, $ */
 'use strict';
 
 var socket = io();
@@ -8,10 +8,15 @@ socket.on('shaked', function(shaked){
   var user = shaked.user;
   console.log(user.id, user.count);
   users[user.id] = { name: user.name, count: user.count };
+  $('#'+user.id).html(formatList(user.name, user.count));
 });
 
 socket.on('newUser', function(userData){
   console.log('Here comes a new challenger', userData);
+  $('#users')
+  .append('<li id="' + userData.id + '">' + formatList(userData.name, userData.count) + '</li>');
 });
 
-
+function formatList(name, count){
+  return name + ' : ' + count;
+}
