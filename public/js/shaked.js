0772a7d585645def2441fa153a8342c54c20508e
Yuki Shichiku <yuuki.79@gmail.com>
2014-06-23 23:53:21 +0900

    Added count down and winner

--- public/js/shaked.js
+++ public/js/shaked.js
@@ -17,6 +17,19 @@ socket.on('newUser', function(userData){
   .append('<li id="' + userData.id + '"><img src="https://graph.facebook.com/' + userData.id + '/picture?height=300" />' + formatList(userData.name, userData.count) + '</li>');
 });
 
+socket.on('countDown', function(res){
+  $('#countDown').html(res.countTime);
+});
+
+socket.on('playTime', function(res){
+  $('#playTime').html(res.playTime);
+});
+
+socket.on('finish', function(winner){
+console.log(winner);
+  $('#winner').html(winner.name);
+});
+
 socket.on('user left', function(user){
   $('#'+user.id).remove();
 });
