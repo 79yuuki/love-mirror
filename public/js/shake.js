d23a9a8933f55c1e9f6bf9c1ee6c849c66ada376
Yuki Shichiku <yuuki.79@gmail.com>
2014-06-23 22:47:59 +0900

    Add shake counter

--- public/js/shake.js
+++ public/js/shake.js
@@ -1,8 +1,16 @@
-/* global io, userId, userName  */
+/* global io, userId, userName, $  */
 'use strict';
 
 var socket = io();
 var count = 0;
+
+(function(){
+  console.log(userId, userName);
+  if (userId && userName) {
+    socket.emit('newUser', { id: userId, name: userName});
+    countUp(0);
+  }
+})();
 
 window.addEventListener('devicemotion', function(event) {
   var gv = event.accelerationIncludingGravity;
@@ -14,5 +22,10 @@ window.addEventListener('devicemotion', function(event) {
       z: gv.z,
       user: { id: userId, name: userName, count: count }
     });
+    countUp(count);
   }
 });
+
+function countUp(count){
+  $('#count').html(count);
+}
